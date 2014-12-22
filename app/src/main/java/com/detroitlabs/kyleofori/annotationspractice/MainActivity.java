4c3582e2c635d01b8a709e1801c8269fcdcd0f1c
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-22 11:56:14 -0500

    Converting home page to annotations--button changes text

--- app/src/main/java/com/detroitlabs/kyleofori/annotationspractice/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/annotationspractice/MainActivity.java
@@ -1,25 +1,32 @@
 package com.detroitlabs.kyleofori.annotationspractice;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_main)
+@EActivity (R.layout.activity_homepage)
 public class MainActivity extends ActionBarActivity {
 
-    @ViewById(R.id.myInput)
-    EditText myInput;
+    @ViewById (R.id.resource_loading_status)
+    TextView txtResourceLoadingStatus;
 
-    @ViewById(R.id.myTextView)
-    TextView textView;
+    @ViewById (R.id.btn_go_to_search)
+    Button btnGoToSearch;
 
-    @Click
-    void myButton() {
-        String name = myInput.getText().toString();
-        textView.setText("Hello " + name);
+    @Click (R.id.btn_go_to_search)
+        void updateButtonText() {
+        txtResourceLoadingStatus.setText(getString(R.string.resources_loaded));
+
     }
 }
