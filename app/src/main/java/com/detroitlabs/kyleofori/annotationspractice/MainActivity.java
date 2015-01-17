4759e872e698b46ceeef1b73a06304f16f1b8fb7
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-01-17 16:18:54 -0500

    Put in an indeterminate progress bar

--- app/src/main/java/com/detroitlabs/kyleofori/annotationspractice/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/annotationspractice/MainActivity.java
@@ -3,17 +3,11 @@ package com.detroitlabs.kyleofori.annotationspractice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.detroitlabs.kyleofori.annotationspractice.KhanAcademyApi;
-import com.detroitlabs.kyleofori.annotationspractice.KhanAcademyApiCallback;
-import com.detroitlabs.kyleofori.annotationspractice.KhanAcademyJSONParser;
-import com.detroitlabs.kyleofori.annotationspractice.Lesson;
-import com.detroitlabs.kyleofori.annotationspractice.R;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
@@ -34,6 +28,9 @@ public class MainActivity extends ActionBarActivity implements KhanAcademyApiCal
     @ViewById (R.id.btn_go_to_search)
     Button btnGoToSearch;
 
+    @ViewById (R.id.progress_wheel)
+    ProgressBar progressWheel;
+
     @Click (R.id.btn_go_to_search)
     void openSearchResultsListFragment() {
         Intent toResultsActivity = new Intent(this, ResultsActivity_.class);
@@ -49,6 +46,7 @@ public class MainActivity extends ActionBarActivity implements KhanAcademyApiCal
     @Override
     public void onSuccess(JSONArray response) {
         txtResourceLoadingStatus.setText(R.string.resources_loaded);
+        progressWheel.setVisibility(View.GONE);
         khanAcademyLessons = KhanAcademyJSONParser.parseJSONObject(response);
     }
 
